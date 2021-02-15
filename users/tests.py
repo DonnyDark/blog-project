4c185ed60464c6f9ae44363276f258aaff928221
DonnyDark <no_game_no_life2k@mail.ru>
2021-02-15 16:51:42 +0600

    users unittests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import CustomUser
 
-# Create your tests here.
+
+class CustomUserTestCase(TestCase):
+
+    def setUp(self):
+        CustomUser.objects.create_user(username='User', email='User@mail.com', password='UserPass')
+        CustomUser.objects.create_superuser(username='Superuser', email='Superuser@mail.com', password='SuperuserPass')
+
+    def test_user(self):
+        user = CustomUser.objects.get(username='User')
+        self.assertEqual(user.email, 'User@mail.com')
+        self.assertNotEqual(user.password, 'UserPass')
+        self.assertEqual(user.is_active, True)
+        self.assertEqual(user.is_admin, False)
+
+    def test_superuser(self):
+        superuser = CustomUser.objects.get(username='Superuser')
+        self.assertEqual(superuser.email, 'Superuser@mail.com')
+        self.assertNotEqual(superuser.password, 'SuperuserPass')
+        self.assertEqual(superuser.is_active, True)
+        self.assertEqual(superuser.is_admin, True)
