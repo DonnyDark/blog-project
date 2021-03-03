a0c05711cac9ce8fcc5a4db2b94665b997b5e9f1
DonnyDark <no_game_no_life2k@mail.ru>
2021-03-03 16:41:24 +0600

    security proxy ssl

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -161,6 +161,7 @@ if ENVIRONMENT == 'production':
     SECURE_CONTENT_TYPE_NOSNIFF = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Heroku
 import dj_database_url
